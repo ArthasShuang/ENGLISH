bb7b556d8e49646b5a5e57faa245b2d264d7e39a
Qin He(Lily) <45560662+LilyHeAsamiko@users.noreply.github.com>
2022-12-21 17:16:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,31 @@ def CARD(card):
   card += 1
   return card
 
-  
+def W5uGe(digitNum):
+  Num = int(digitNum)
+  if len(digitNum) >1:
+    Num = int(digitNum[0])
+  if Num == 1 or Num == 2:
+    W5uGe == 'Mu'
+  elif Num == 3 or Num == 4:
+    W5uGe == 'Shui'
+  elif Num == 5 or Num == 6:
+    W5uGe == 'Tu'
+  elif Num == 7 or Num == 8:
+    W5uGe == 'Jin'
+  elif Num == 9 or Num == 0:
+    W5uGe == 'Shui'
+  else：
+    assert(W5uGe<= 9, 'W5uGe is supposed to be one single digit number!')
+  return W5uGe
+
+def S3anCai(TianGC, RenGC, DiGC):
+  Frst = W5uGe(int(TianGC))
+  Scd = W5uGe(int(RenGC))
+  Thrd = W5uGe(int(DiGC))
+  S3anCai = [Frst, Scd, Thrd]
+  return S3anCai
+
 #创建flask duixiang
 app = Flask(__name__)
 
@@ -76,19 +100,68 @@ def w5ge3cai():
   if len(kd) == 2:
     surname = kd[0]
     lastname = kd[1]
+    TianG = int(surname) + 1
+    RenG = int(surname) + int(lastname)
+    DiG = int(lastname) + 1
+    ZongG = int(surname) + int(lastname)
+    WaiG =  2
+  
   elif len(kd) == 3:
     surname = kd[0]
     middlename = kd[1]
     lastname = kd[2]
+    TianG = int(surname) + 1
+    RenG = int(surname) + int(lastname)
+    DiG = int(lastname) + int(middlename)
+    ZongG = int(surname) + int(middlename) + int(lastname)
+    WaiG =  int(lastname) + 1
+    
   elif len(kd) == 4:
-    surname = kd[0]+kd[1]
+    surname = int(kd[0])+int(kd[1])
     middlename = kd[2]
-    lastname = kd[3]  
+    lastname = kd[3]
+    TianG = surname+1
+    RenG = int(surname) + int(lastname)
+    DiG = int(lastname) + int(middlename)
+    ZongG = int(surname) + int(middlename) + int(lastname)
+    WaiG =  int(lastname) + 1
+    
   else:
-    c0 = 
-  return render_template("w5ge3cai.html")
-
-@app.route("/meihuyishu")
+    return('Wrong input!')
+   
+  if len(TianG) >1:
+      TianGC = TianG[0]
+  else:
+      TianGC = TianG
+  TianGCW = W5uGe(TianGC)
+  if len(RenG) >1:
+    RenGC = RenG[0]
+  else:
+    RenGC =RenG 
+  RenGCW = W5uGe(RenGC)
+  if len(DiG) >1:
+    DiGC = DiG[0]
+  else:
+    DiGC =DiG  
+  DiGCW = W5uGe(DiGC)
+  if len(WaiG) >1:
+    WaiGC = WaiG[0]
+  else:
+    WaiGC =WaiG  
+  WaiGCW = W5uGe(WaiGC)
+  if len(ZongG) >1:
+    ZongC = ZongG[0]
+  else:
+    ZongGC =ZongG  
+  ZongCW = W5uGe(ZongGC)
+  W5uG = [TianGCW, RenGCW, DiGCW, WaiGCW, ZongCW]
+  S3C = S3anCai(TianGC, RenGC, DiGC)
+    return render_template("w5ge3cai.html",**{
+  "W5uG": W5uG,
+  "S3C": S3C
+})
+    
+@app.route("/meihuayishu")
 def meihuayishu():  
   return render_template('meihuayishu.html')
 
